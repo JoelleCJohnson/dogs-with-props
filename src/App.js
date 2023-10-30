b6e93ab0389342d490e53bafee59151c2af55416
Joelle  Johnson <joellej221@gmail.com>
2023-10-30 10:38:19 -0400

    dogs with props, conditional styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Dog from './components/Dog';
+
+const dogs = [
+  { name: 'Twinkie', age: 6, breed: 'Pug/Dachshund', deceased: false },
+  { name: 'Winston', age: 15, breed: 'Fox-looking dog', deceased: true },
+  { name: 'Raven', age: 20, breed: 'German shepherd, black lab', deceased: true },
+  { name: 'Daisy', age: 9, breed: 'Morkie', deceased: false},
+]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Dogs:</h1>
+          {dogs.map(dog => <Dog key={dog.name}
+                                age={dog.age} 
+                                name={dog.name} 
+                                breed={dog.breed}
+                                deceased={dog.deceased} />)}
       </header>
     </div>
   );
